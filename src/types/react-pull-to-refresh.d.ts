772c5cd6ca00a5194b9d8ad86a8fb7a84521f1e8
[dyad] <abdullaheltiby@gmail.com>
2025-11-07 20:20:54 +0300

    [dyad] Fixing database function and TypeScript types - wrote 1 file(s), executed 2 SQL queries

---
+++ src/types/react-pull-to-refresh.d.ts
@@ -0,0 +1,17 @@
+declare module 'react-pull-to-refresh' {
+  import * as React from 'react';
+
+  interface PullToRefreshProps {
+    onRefresh: () => Promise<any> | void;
+    children: React.ReactNode;
+    disabled?: boolean;
+    className?: string;
+    icon?: React.ReactNode;
+    loading?: React.ReactNode;
+    resistance?: number;
+    isRefreshing?: boolean;
+  }
+
+  const PullToRefresh: React.FC<PullToRefreshProps>;
+  export default PullToRefresh;
+}
